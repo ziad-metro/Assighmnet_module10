885d1b1dec33c8d7b09d2264f2dae85e3ab7bfb2
ziad-metro <152270838+ziad-metro@users.noreply.github.com>
2025-02-05 10:14:20 +0600

    Ostad_ module10_assighnment

---
+++ assighnment_2.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ContactApp());
+}
+
+class ContactApp extends StatelessWidget {
+  const ContactApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ContactListScreen(),
+    );
+  }
+}
+
+class ContactListScreen extends StatefulWidget {
+  const ContactListScreen({super.key});
+
+  @override
+  State<ContactListScreen> createState() => _ContactListScreenState();
+}
+
+class _ContactListScreenState extends State<ContactListScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController numberController = TextEditingController();
+  List<Map<String, String>> contacts = [];
+
+  void addContact() {
+    String name = nameController.text.trim();
+    String number = numberController.text.trim();
+    if (name.isNotEmpty && number.isNotEmpty) {
+      setState(() {
+        contacts.add({'name': name, 'number': number});
+      });
+      nameController.clear();
+      numberController.clear();
+    }
+  }
+
+  void deleteContact(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Confirmation"),
+          content: const Text("Are you sure for Delete?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  contacts.removeAt(index);
+                });
+                Navigator.pop(context);
+              },
+              child: const Text("Delete"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact List"),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: "Name"),
+            ),
+            TextField(
+              controller: numberController,
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(labelText: "Number"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: addContact,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[600],
+              ),
+              child: const Text("Add"),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(contacts[index]['name']!),
+                      subtitle: Text(contacts[index]['number']!),
+                      leading: const Icon(Icons.person, color: Colors.red),
+                      trailing: const Icon(Icons.phone, color: Colors.blue),
+                      onLongPress: () => deleteContact(index),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
